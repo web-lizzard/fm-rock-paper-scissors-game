b0cc2c0de85e845110825f3acaea0d354ef64763
Adrian Czarnous <adrian.czarnous@infermedica.com>
2022-11-26 03:47:37 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>ddd</main>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use '@/assets/scss/variables' as *;
+                         @use '@/assets/scss/mixins' as *;
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
